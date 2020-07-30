c8c11af9244f50088d922ea0b0308a91b54aeecd
potatolight <shuguangbai@gmail.com>
2020-07-30 05:38:21 +0000

    fetcherupdate

--- fetcher.js
+++ fetcher.js
@@ -3,9 +3,9 @@ const request = require('request');
 const web = process.argv[2]
 const location = process.argv[3]
 request(web, (error, response, body) => {
-  fs.writeFileSync ('index.html', body)
+  fs.writeFileSync (location, body)
   console.log("File written successfully\n"); 
   console.log("The written has the following contents:"); 
-  console.log(fs.readFileSync("index.html", "utf8")); 
+  console.log(fs.readFileSync(location, "utf8")); 
   console.log('Downloaded and saved '+ body.length + ' bytes to '+ location )  
  });
