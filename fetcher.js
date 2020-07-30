b4b046432e09bc737252c7e584ad1d7aafd8e360
potatolight <shuguangbai@gmail.com>
2020-07-30 05:22:56 +0000

    fetcher

--- fetcher.js
+++ fetcher.js
@@ -9,28 +9,3 @@ request(web, (error, response, body) => {
   console.log(fs.readFileSync("index.html", "utf8")); 
   console.log('Downloaded and saved '+ body.length + ' bytes to '+ location )  
  });
-
-
-
-
-// const writeIn ()
-
-// // asyncBreeds.js
-// const breedDetailsFromFile = function (name, functionToRunWhenThingsAreDone) {
-//   // console.log('breedDetailsFromFile: Calling readFile...');
-//   fs.writeFileSync(`./${name}.html`, 'utf8', (error, data) => {
-//     // CHANGE: Pass data into callback instead of returning it directly
-//     // console.log("In readFile's Callback: it has the data.");
-//     if(error){
-//       functionToRunWhenThingsAreDone(undefined);
-//     }
-//     functionToRunWhenThingsAreDone(data);
-//   });
-// };
-// // CHANGE 1: Moved the console.log into a new function:
-// const printOutCatBreed = (breed)=> {
-//   console.log('Return Value: ', breed) // => print out details correctly.
-// };
-// // CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
-// breedDetailsFromFile('Bombay', printOutCatBreed);
-// // module.exports = breedDetailsFromFile;
